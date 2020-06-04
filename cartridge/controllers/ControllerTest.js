8eb788a21aac48f173ec7b1ec6d2af864af4e249
Mario Murillo <mmurillo@unitedvirtualities.com>
2020-06-04 15:54:05 -0600

    I added a log category

--- cartridge/controllers/ControllerTest.js
+++ cartridge/controllers/ControllerTest.js
@@ -4,6 +4,9 @@ var server = require('server');
 
 server.get('Show', server.middleware.get, function(req, res, next){
         res.render('/helloWorld');
+        var Logger = require('dw/system/Logger'); 
+        var testLogger = Logger.getLogger("MyCustomLog", "logTest"); 
+        testLogger.info("This is a log test from my controller!");
         next();
     });
 
