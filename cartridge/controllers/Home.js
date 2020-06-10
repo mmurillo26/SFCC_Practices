949df9892cbf5289451c52ff8ba0775067b6e25c
Mario Murillo <mmurillo@unitedvirtualities.com>
2020-06-10 17:52:27 -0600

    extending Home controller to render template

---
+++ cartridge/controllers/Home.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var page = require('app_storefront_base/cartridge/controllers/Home');
+var server = require('server');
+
+server.extend(page);
+
+server.replace('Show', server.middleware.get, function(req, res, next){
+    res.render('myNewTemplate');
+    next();
+});
+
+module.exports = server.exports();
