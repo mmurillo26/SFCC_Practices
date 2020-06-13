9f38b98a10419b74228523079796143798404333
Mario Murillo <mmurillo@unitedvirtualities.com>
2020-06-12 19:52:38 -0600

    added form validations

--- cartridge/controllers/ControllerTest.js
+++ cartridge/controllers/ControllerTest.js
@@ -3,10 +3,13 @@
 var server = require('server');
 
 server.get('Show', server.middleware.get, function(req, res, next){
-        res.render('/helloWorld');
         var Logger = require('dw/system/Logger'); 
         var testLogger = Logger.getLogger("MyCustomLog", "logTest"); 
         testLogger.info("This is a log test from my controller!");
+        var testLogger2 = Logger.getLogger("logMario", "logChido");
+        testLogger2.info("this is another log test");
+        var formTest = server.forms.getForm('formTest');
+        res.render('/helloWorld', {form: formTest});
         next();
     });
 
