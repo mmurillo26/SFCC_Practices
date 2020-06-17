bbf070aaeb8fa99955022012d57f7e9b9d7477ca
Mario Murillo <mmurillo@unitedvirtualities.com>
2020-06-16 18:51:16 -0600

    updated ControllerTest.js

--- cartridge/controllers/ControllerTest.js
+++ cartridge/controllers/ControllerTest.js
@@ -1,15 +1,25 @@
 'use strict';
 
 var server = require('server');
+var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 
-server.get('Show', server.middleware.get, function(req, res, next){
-        var Logger = require('dw/system/Logger'); 
-        var testLogger = Logger.getLogger("MyCustomLog", "logTest"); 
+server.get('Show', server.middleware.get, csrfProtection.generateToken, function(req, res, next){
+        var Logger = require('dw/system/Logger');
+
+        var testLogger = Logger.getLogger("logInfo", "logInfoTest"),
+        testLogger2 = Logger.getLogger("logWarn", "logWarnTest"),
+        testLogger3 = Logger.getLogger("logError", "logErrorTest"),
+        testLogger4 = Logger.getLogger("logDebug", "logDebugTest");
+
         testLogger.info("This is a log test from my controller!");
-        var testLogger2 = Logger.getLogger("logMario", "logChido");
-        testLogger2.info("this is another log test");
+        testLogger2.warn("this is another log test");
+        testLogger3.error("this is another log test");
+        testLogger4.debug("this is another log test");
+
         var formTest = server.forms.getForm('formTest');
+
         res.render('/helloWorld', {form: formTest});
+
         next();
     });
 
